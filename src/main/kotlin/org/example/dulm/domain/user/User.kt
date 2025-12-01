b777049122eb0f457b9f75cf2509442046efd270
imhanseong <hans.hanseong@gmail.com>
2025-12-01 20:17:08 +0900

    feat :: User 도메인 생성

---
+++ src/main/kotlin/org/example/dulm/domain/user/User.kt
@@ -0,0 +1,12 @@
+package org.example.dulm.domain.user
+
+import java.time.LocalDateTime
+
+data class User (
+    val id : Long? = null,
+    val email : String,
+    val password : String,
+    val nickname : String,
+    val createdAt : LocalDateTime = LocalDateTime.now(),
+    val updatedAt : LocalDateTime = LocalDateTime.now()
+)
