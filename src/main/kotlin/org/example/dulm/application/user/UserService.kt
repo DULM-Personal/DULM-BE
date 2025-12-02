e0c43268fc09084b8c2707ac2c726b98e832be81
imhanseong <hans.hanseong@gmail.com>
2025-12-02 11:18:56 +0900

    feat :: UserService 생성

---
+++ src/main/kotlin/org/example/dulm/application/user/UserService.kt
@@ -0,0 +1,38 @@
+package org.example.dulm.application.user
+
+import org.example.dulm.application.user.port.`in`.UserUseCase
+import org.example.dulm.application.user.port.out.UserRepository
+import org.example.dulm.domain.user.User
+import org.example.dulm.exception.BaseException
+import org.example.dulm.exception.ErrorCode
+import org.example.dulm.presentation.dto.response.UserResponse
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+
+@Service
+class UserService (
+    private val userRepository : UserRepository,
+    private val passwordEncoder : PasswordEncoder
+): UserUseCase{
+
+    override fun signUp(email: String, password: String, nickname: String): UserResponse {
+        if (userRepository.findByEmail(email) != null) {
+            throw BaseException(ErrorCode.EMAIL_ALREADY_EXISTS)
+        }
+
+        val encryptedPassword = passwordEncoder.encode(password)
+
+        val user = User(email = email, password = password, nickname = nickname)
+        val savedUser = userRepository.save(user)
+
+        return UserResponse.from(savedUser)
+    }
+
+    override fun getUser(id: Long): UserResponse {
+        val user = userRepository.findById(id)
+            ?: throw BaseException(ErrorCode.USER_NOT_FOUND)
+
+        return UserResponse.from(user)
+    }
+
+}
