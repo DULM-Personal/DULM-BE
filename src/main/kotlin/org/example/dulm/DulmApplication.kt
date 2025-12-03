6712cb80a52e6274b83bbae2f32e8413d2974db5
imhanseong <hans.hanseong@gmail.com>
2025-12-03 21:42:09 +0900

    feat :: 어노테이션 추가

--- src/main/kotlin/org/example/dulm/DulmApplication.kt
+++ src/main/kotlin/org/example/dulm/DulmApplication.kt
@@ -1,9 +1,15 @@
 package org.example.dulm
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableCaching
+@ConfigurationPropertiesScan
+@EnableScheduling
 class DulmApplication
 
 fun main(args: Array<String>) {
