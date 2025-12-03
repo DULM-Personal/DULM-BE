6537956c8fd60e772bd283f7c920cd0d17f7e371
imhanseong <hans.hanseong@gmail.com>
2025-12-03 21:42:20 +0900

    feat :: RefreshToken 추가

---
+++ src/main/kotlin/org/example/dulm/global/jwt/entity/RefreshToken.kt
@@ -0,0 +1,23 @@
+package org.example.dulm.global.jwt.entity
+
+import jakarta.persistence.Id
+import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.TimeToLive
+import org.springframework.data.redis.core.index.Indexed
+
+@RedisHash(value = "refresh")
+class RefreshToken(
+    @Id
+    val id: String,
+
+    @Indexed
+    var token: String,
+
+    @TimeToLive
+    var ttl: Long
+) {
+    fun update(token: String?, ttl: Long) {
+        this.token = token!!
+        this.ttl = ttl
+    }
+}
