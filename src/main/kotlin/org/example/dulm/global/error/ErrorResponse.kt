7b0d144ff72d4f5e7e559bd76e5c03aa8c1c8dad
imhanseong <hans.hanseong@gmail.com>
2025-12-07 23:14:56 +0900

    fix :: ErrorResponse 수정

--- src/main/kotlin/org/example/dulm/global/error/ErrorResponse.kt
+++ src/main/kotlin/org/example/dulm/global/error/ErrorResponse.kt
@@ -1,6 +1,16 @@
 package org.example.dulm.global.error
 
-data class ErrorResponse (
+import org.example.dulm.global.error.exception.ErrorCode
+
+data class ErrorResponse(
     val status: Int,
     val message: String
-)
+) {
+    companion object {
+        fun from(errorCode: ErrorCode): ErrorResponse =
+            ErrorResponse(
+                errorCode.status.value(),
+                errorCode.message
+            )
+    }
+}
