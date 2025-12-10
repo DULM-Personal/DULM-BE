e4272b3e3188213b0c3ba2f0a4bd7a2a714955e3
imhanseong <hans.hanseong@gmail.com>
2025-12-10 21:13:20 +0900

    fix :: User Repository 수정

--- src/main/kotlin/org/example/dulm/domain/user/application/port/out/UserRepository.kt
+++ src/main/kotlin/org/example/dulm/domain/user/application/port/out/UserRepository.kt
@@ -1,9 +1,11 @@
 package org.example.dulm.domain.user.application.port.out
 
 import org.example.dulm.domain.user.domain.User
+import org.example.dulm.domain.user.entity.UserJpaEntity
+import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository {
+interface UserRepository : JpaRepository<UserJpaEntity, Long> {
     fun save(user: User): User
     fun findByEmail(email: String): User?
-    fun findById(id: Long): User?
+    fun existsByEmail(email: String): Boolean
 }
