3d7f10db1fa1dce84d12f43c27db9c79e1681799
imhanseong <hans.hanseong@gmail.com>
2025-12-10 21:14:24 +0900

    feat :: AuthDetailsService 추가

---
+++ src/main/kotlin/org/example/dulm/global/security/auth/AuthDetailsService.kt
@@ -0,0 +1,20 @@
+package org.example.dulm.global.security.auth
+
+import org.example.dulm.domain.user.application.port.out.UserRepository
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+@Service
+class AuthDetailsService(
+    private val userRepository: UserRepository
+) : UserDetailsService {
+
+    override fun loadUserByUsername(email: String): UserDetails {
+        val user = userRepository.findByEmail(email)
+            ?: throw UsernameNotFoundException("User not found")
+
+        return AuthDetails(userId = user.id!!)
+    }
+}
