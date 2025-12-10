fc71d0dc275113320c0003fdd824811b217d5fe6
imhanseong <hans.hanseong@gmail.com>
2025-12-10 21:13:34 +0900

    fix :: User 수정

--- src/main/kotlin/org/example/dulm/domain/user/domain/User.kt
+++ src/main/kotlin/org/example/dulm/domain/user/domain/User.kt
@@ -3,10 +3,10 @@ package org.example.dulm.domain.user.domain
 import java.time.LocalDateTime
 
 data class User (
-    val id : Long? = null,
-    val email : String,
-    val password : String,
-    val nickname : String,
-    val createdAt : LocalDateTime = LocalDateTime.now(),
-    val updatedAt : LocalDateTime = LocalDateTime.now()
+    val id: Long? = null,
+    val email: String,
+    val password: String?,
+    val nickname: String,
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    val updatedAt: LocalDateTime = LocalDateTime.now()
 )
