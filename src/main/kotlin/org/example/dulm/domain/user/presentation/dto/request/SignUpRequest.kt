b181c11da6a9b322e3d462eed40db075749708aa
imhanseong <hans.hanseong@gmail.com>
2026-01-05 10:00:35 +0900

    fix :: SignUpRequest

--- src/main/kotlin/org/example/dulm/domain/user/presentation/dto/request/SignUpRequest.kt
+++ src/main/kotlin/org/example/dulm/domain/user/presentation/dto/request/SignUpRequest.kt
@@ -3,16 +3,13 @@ package org.example.dulm.domain.user.presentation.dto.request
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
 
-data class SignUpRequest (
-
+data class SignUpRequest(
     @field:Email(message = "이메일 형식이 올바르지 않습니다")
     @field:NotBlank(message = "이메일이 비어있을 수 없습니다")
-    val email : String,
-
+    val email: String,
+    val code: String,
     @field:NotBlank(message = "비밀번호는 비어 있을  수 없습니다.")
-    val password : String,
-
+    val password: String,
     @field:NotBlank(message = "닉네임은 비어 있을 수 없습니다.")
-    val nickname : String
-
+    val nickname: String,
 )
