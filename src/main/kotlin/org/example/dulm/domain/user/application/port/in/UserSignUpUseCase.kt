9c65f5daa983745142ff0405749d7be8950aa2bb
imhanseong <hans.hanseong@gmail.com>
2026-01-05 09:59:41 +0900

    feat :: UserSignUpUseCase

--- src/main/kotlin/org/example/dulm/domain/user/application/port/in/UserSignUpUseCase.kt
+++ src/main/kotlin/org/example/dulm/domain/user/application/port/in/UserSignUpUseCase.kt
@@ -4,5 +4,5 @@ import org.example.dulm.domain.user.presentation.dto.request.SignUpRequest
 import org.example.dulm.global.security.jwt.dto.TokenResponse
 
 interface UserSignUpUseCase {
-    fun execute(request: SignUpRequest) : TokenResponse
+    fun execute(request: SignUpRequest): TokenResponse
 }
