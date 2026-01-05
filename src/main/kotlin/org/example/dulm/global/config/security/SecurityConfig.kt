1b934b93c6a48897c645911df5bb17bdd87bf5aa
imhanseong <hans.hanseong@gmail.com>
2026-01-05 10:01:38 +0900

    refactor :: SecurityConfig

--- src/main/kotlin/org/example/dulm/global/config/security/SecurityConfig.kt
+++ src/main/kotlin/org/example/dulm/global/config/security/SecurityConfig.kt
@@ -1,5 +1,6 @@
-package org.example.dulm.global.config
+package org.example.dulm.global.config.security
 
+import org.example.dulm.global.security.jwt.JwtProperties
 import org.example.dulm.global.security.jwt.JwtTokenFilter
 import org.example.dulm.global.security.jwt.JwtTokenProvider
 import org.springframework.context.annotation.Bean
@@ -9,7 +10,6 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
@@ -19,37 +19,30 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 class SecurityConfig(
     private val jwtTokenProvider: JwtTokenProvider,
-    private val userDetailsService: UserDetailsService
+    private val jwtProperties: JwtProperties,
 ) {
-
     // 1) PasswordEncoder 등록
     @Bean
     fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 
     // 2) AuthenticationManager 등록
     @Bean
-    fun authenticationManager(
-        config: AuthenticationConfiguration
-    ): AuthenticationManager = config.authenticationManager
+    fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager = config.authenticationManager
 
     // 3) SecurityFilterChain 설정 (가장 중요)
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
-
         http
             .csrf { it.disable() } // JWT에서는 CSRF 불필요
-
             .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
-
             .authorizeHttpRequests { auth ->
                 auth
-                    .requestMatchers("/auth/**").permitAll()   // 로그인, 회원가입
-                    .anyRequest().authenticated()               // 나머지는 인증 필요
+                    .requestMatchers("/user/**").permitAll() // 로그인, 회원가입
+                    .anyRequest().authenticated() // 나머지는 인증 필요
             }
-
             .addFilterBefore(
-                JwtTokenFilter(jwtTokenProvider),
-                UsernamePasswordAuthenticationFilter::class.java
+                JwtTokenFilter(jwtTokenProvider, jwtProperties),
+                UsernamePasswordAuthenticationFilter::class.java,
             )
 
         return http.build()
