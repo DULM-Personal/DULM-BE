8445fd8949c5d06363747da185135f6a2ae69a78
imhanseong <hans.hanseong@gmail.com>
2026-01-05 10:03:22 +0900

    fix :: AuthDetails

--- src/main/kotlin/org/example/dulm/global/security/auth/AuthDetails.kt
+++ src/main/kotlin/org/example/dulm/global/security/auth/AuthDetails.kt
@@ -5,14 +5,18 @@ import org.springframework.security.core.userdetails.UserDetails
 
 class AuthDetails(
     private val userId: Long,
-): UserDetails {
-
+) : UserDetails {
     override fun getUsername(): String = userId.toString()
-    override fun getAuthorities(): Collection<out GrantedAuthority> = emptyList()
-    override fun getPassword(): String? = null
-    override fun isAccountNonExpired(): Boolean = true
-    override fun isAccountNonLocked(): Boolean = true
-    override fun isCredentialsNonExpired(): Boolean = true
-    override fun isEnabled(): Boolean = true
 
+    override fun getAuthorities(): Collection<out GrantedAuthority> = emptyList()
+
+    override fun getPassword(): String? = null
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isEnabled(): Boolean = true
 }
