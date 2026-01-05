916ff310450715230d8b079dd6b0df6007d7e0ee
imhanseong <hans.hanseong@gmail.com>
2026-01-05 10:01:51 +0900

    fix :: ErrorResponse

--- src/main/kotlin/org/example/dulm/global/error/ErrorResponse.kt
+++ src/main/kotlin/org/example/dulm/global/error/ErrorResponse.kt
@@ -4,13 +4,13 @@ import org.example.dulm.global.error.exception.ErrorCode
 
 data class ErrorResponse(
     val status: Int,
-    val message: String
+    val message: String,
 ) {
     companion object {
         fun from(errorCode: ErrorCode): ErrorResponse =
             ErrorResponse(
                 errorCode.status.value(),
-                errorCode.message
+                errorCode.message,
             )
     }
 }
