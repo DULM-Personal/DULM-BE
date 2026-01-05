2921a92b79d0b826f0c8b302ad500c8aa4ea182e
imhanseong <hans.hanseong@gmail.com>
2026-01-05 10:02:19 +0900

    fix :: GobalExceptionHandler

--- src/main/kotlin/org/example/dulm/global/error/GlobalExceptionHandler.kt
+++ src/main/kotlin/org/example/dulm/global/error/GlobalExceptionHandler.kt
@@ -1,14 +1,12 @@
 package org.example.dulm.global.error
 
 import org.example.dulm.global.error.exception.DulmException
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
-
     @ExceptionHandler(DulmException::class)
     fun handleException(e: DulmException): ResponseEntity<ErrorResponse> {
         val code = e.errorCode
