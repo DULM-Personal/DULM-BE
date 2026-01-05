2d8df2d00d5ef5a0e5546cf6cda1053c38e1ad63
imhanseong <hans.hanseong@gmail.com>
2026-01-05 10:00:19 +0900

    fix :: LoginRequest

--- src/main/kotlin/org/example/dulm/domain/user/presentation/dto/request/LoginRequest.kt
+++ src/main/kotlin/org/example/dulm/domain/user/presentation/dto/request/LoginRequest.kt
@@ -1,6 +1,6 @@
 package org.example.dulm.domain.user.presentation.dto.request
 
-data class LoginRequest (
-    val email: String,
-    val password: String
+data class LoginRequest(
+    val nickname: String,
+    val password: String,
 )
