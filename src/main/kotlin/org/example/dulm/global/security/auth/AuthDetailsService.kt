1172ac685a0ead661746cff92d231c027904a52e
imhanseong <hans.hanseong@gmail.com>
2026-01-05 10:03:33 +0900

    fix :: AuthDetailsService

--- src/main/kotlin/org/example/dulm/global/security/auth/AuthDetailsService.kt
+++ src/main/kotlin/org/example/dulm/global/security/auth/AuthDetailsService.kt
@@ -1,6 +1,6 @@
 package org.example.dulm.global.security.auth
 
-import org.example.dulm.domain.user.application.port.out.UserRepository
+import org.example.dulm.domain.user.application.port.out.repository.UserRepository
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
@@ -8,12 +8,12 @@ import org.springframework.stereotype.Service
 
 @Service
 class AuthDetailsService(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
 ) : UserDetailsService {
-
     override fun loadUserByUsername(email: String): UserDetails {
-        val user = userRepository.findByEmail(email)
-            ?: throw UsernameNotFoundException("User not found")
+        val user =
+            userRepository.findByEmail(email)
+                ?: throw UsernameNotFoundException("User not found")
 
         return AuthDetails(userId = user.id!!)
     }
