45d3dce50000a413ef8de873eb13d444eace6364
imhanseong <hans.hanseong@gmail.com>
2026-01-05 10:04:30 +0900

    fix :: JwtTokenProvider

--- src/main/kotlin/org/example/dulm/global/security/jwt/JwtTokenProvider.kt
+++ src/main/kotlin/org/example/dulm/global/security/jwt/JwtTokenProvider.kt
@@ -12,23 +12,22 @@ import javax.crypto.spec.SecretKeySpec
 
 @Component
 class JwtTokenProvider(
-    private val jwtProperties: JwtProperties
+    private val jwtProperties: JwtProperties,
 ) {
-
     private val secretKey: SecretKey =
         SecretKeySpec(
             jwtProperties.secretKey.toByteArray(),
-            SignatureAlgorithm.HS256.jcaName
+            SignatureAlgorithm.HS256.jcaName,
         )
 
+    fun createAccessToken(email: String): String = createToken(email, jwtProperties.accessExp)
 
-    fun createAccessToken(email: String): String =
-        createToken(email, jwtProperties.accessExp)
+    fun createRefreshToken(email: String): String = createToken(email, jwtProperties.refreshExp)
 
-    fun createRefreshToken(email: String): String =
-        createToken(email, jwtProperties.refreshExp)
-
-    private fun createToken(email: String, expiration: Long): String {
+    private fun createToken(
+        email: String,
+        expiration: Long,
+    ): String {
         val now = Date()
         return Jwts.builder()
             .setSubject(email)
