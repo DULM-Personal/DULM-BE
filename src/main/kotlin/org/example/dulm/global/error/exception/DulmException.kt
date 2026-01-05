c19adbcfe007352d58abe39d1b5811a73786bddc
imhanseong <hans.hanseong@gmail.com>
2026-01-05 10:02:01 +0900

    fix :: DulmException

--- src/main/kotlin/org/example/dulm/global/error/exception/DulmException.kt
+++ src/main/kotlin/org/example/dulm/global/error/exception/DulmException.kt
@@ -1,7 +1,5 @@
 package org.example.dulm.global.error.exception
 
-import org.example.dulm.global.error.exception.ErrorCode
-
-open class DulmException (
-    val errorCode : ErrorCode
+open class DulmException(
+    val errorCode: ErrorCode,
 ) : RuntimeException()
