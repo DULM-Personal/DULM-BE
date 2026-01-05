5f830122abf2d9b8f89d3ae81a330ada5fed089c
imhanseong <hans.hanseong@gmail.com>
2026-01-05 10:00:46 +0900

    fix :: UserResponse

--- src/main/kotlin/org/example/dulm/domain/user/presentation/dto/response/UserResponse.kt
+++ src/main/kotlin/org/example/dulm/domain/user/presentation/dto/response/UserResponse.kt
@@ -2,16 +2,17 @@ package org.example.dulm.domain.user.presentation.dto.response
 
 import org.example.dulm.domain.user.domain.User
 
-data class UserResponse (
-    val id : Long,
-    val email : String,
-    val nickname : String,
+data class UserResponse(
+    val id: Long,
+    val email: String,
+    val nickname: String,
 ) {
     companion object {
-        fun from(user: User) = UserResponse(
-            id = user.id!!,
-            email = user.email,
-            nickname = user.nickname
-        )
+        fun from(user: User) =
+            UserResponse(
+                id = user.id!!,
+                email = user.email,
+                nickname = user.nickname,
+            )
     }
 }
