6e15d5f70fc7419ffbc80bc4eb7431a37226c769
imhanseong <hans.hanseong@gmail.com>
2026-01-05 10:01:18 +0900

    fix :: UserSignUpService

--- src/main/kotlin/org/example/dulm/domain/user/service/UserSignUpService.kt
+++ src/main/kotlin/org/example/dulm/domain/user/service/UserSignUpService.kt
@@ -1,7 +1,7 @@
 package org.example.dulm.domain.user.service
 
 import org.example.dulm.domain.user.application.port.`in`.UserSignUpUseCase
-import org.example.dulm.domain.user.application.port.out.UserRepository
+import org.example.dulm.domain.user.application.port.out.repository.UserRepository
 import org.example.dulm.domain.user.domain.User
 import org.example.dulm.domain.user.entity.UserJpaEntity
 import org.example.dulm.domain.user.presentation.dto.request.SignUpRequest
@@ -12,42 +12,44 @@ import org.example.dulm.global.security.jwt.dto.TokenResponse
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
-
 @Service
 class UserSignUpService(
     private val userRepository: UserRepository,
     private val passwordEncoder: PasswordEncoder,
-    private val jwtTokenProvider: JwtTokenProvider
-): UserSignUpUseCase {
-
+    private val jwtTokenProvider: JwtTokenProvider,
+) : UserSignUpUseCase {
     override fun execute(request: SignUpRequest): TokenResponse {
         // 1. 이메일 중복 확인
         if (userRepository.existsByEmail(request.email)) {
             throw DulmException(ErrorCode.EMAIL_ALREADY_EXISTS)
         }
+        // 2. 닉네임 중복 확인
+        if (userRepository.existsByNickname(request.nickname)) {
+            throw DulmException(ErrorCode.NICKNAME_ALREADY_EXISTS)
+        }
 
-        // 2. 비밀번호 암호화
+        // 4. 비밀번호 암호화
         val encodedPassword = passwordEncoder.encode(request.password)
 
-        // 3. 도메인 객체 생성
-        val user = User(
-            email = request.email,
-            password = encodedPassword,
-            nickname = request.nickname
-        )
+        // 5. 도메인 객체 생성
+        val user =
+            User(
+                email = request.email,
+                password = encodedPassword,
+                nickname = request.nickname,
+            )
 
-        // 4. DB 저장
+        // 6. DB 저장
         val saved = userRepository.save(UserJpaEntity.from(user))
 
-        // 5. 토큰 생성 (email 기반)
+        // 토큰 발급
         val access = jwtTokenProvider.createAccessToken(saved.email)
         val refresh = jwtTokenProvider.createRefreshToken(saved.email)
 
-        // 6. 토큰 응답 반환 없어서 오류났던 부분
+        // 9. 토큰 응답 반환 없어서 오류났던 부분
         return TokenResponse(
             accessToken = access,
-            refreshToken = refresh
+            refreshToken = refresh,
         )
     }
 }
-
