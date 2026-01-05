0b69a0c8159a454340350ea46edb7480c6da7ed0
imhanseong <hans.hanseong@gmail.com>
2026-01-05 09:59:53 +0900

    refactor :: UserRepository

--- src/main/kotlin/org/example/dulm/domain/user/application/port/out/repository/UserRepository.kt
+++ src/main/kotlin/org/example/dulm/domain/user/application/port/out/repository/UserRepository.kt
@@ -1,4 +1,4 @@
-package org.example.dulm.domain.user.application.port.out
+package org.example.dulm.domain.user.application.port.out.repository
 
 import org.example.dulm.domain.user.domain.User
 import org.example.dulm.domain.user.entity.UserJpaEntity
@@ -6,6 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository : JpaRepository<UserJpaEntity, Long> {
     fun save(user: User): User
+
     fun findByEmail(email: String): User?
+
+    fun findByNickname(nickname: String): UserJpaEntity?
+
     fun existsByEmail(email: String): Boolean
+
+    fun existsByNickname(nickname: String): Boolean
 }
