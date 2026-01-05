8165b4b0cb657c51f2a66494d191ec5c39fe32b4
imhanseong <hans.hanseong@gmail.com>
2026-01-05 10:00:00 +0900

    fix :: User

--- src/main/kotlin/org/example/dulm/domain/user/domain/User.kt
+++ src/main/kotlin/org/example/dulm/domain/user/domain/User.kt
@@ -2,11 +2,11 @@ package org.example.dulm.domain.user.domain
 
 import java.time.LocalDateTime
 
-data class User (
+data class User(
     val id: Long? = null,
     val email: String,
     val password: String?,
     val nickname: String,
     val createdAt: LocalDateTime = LocalDateTime.now(),
-    val updatedAt: LocalDateTime = LocalDateTime.now()
+    val updatedAt: LocalDateTime = LocalDateTime.now(),
 )
